da67699c1b084a1404cdc208799b88ecf4ce1bdd
user <user@account.com>
2024-04-27 11:15:49 +0200

    Initial Commit

---
+++ Lab2_mc/Lab2_mcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_mcApp.swift
+//  Lab2_mc
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_mcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
